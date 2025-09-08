da610c3e3afb61a92a52a5a240b689f6c1868e94
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-08 19:50:46 +0300

    Add methods: push and forEach in prototype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 'use strict';
 
+// function MyArray(...args) {
+//     this.length = args.length;
+//     for (let i = 0; i < args.length; i++) {
+//         this[i] = args[i];
+//     }
+// }
+
+// MyArray.prototype.flat = function (depth = 1) {
+//     const flatResult = new MyArray();
+
+//     function flatten(source, currentDepth) {
+//         for (let i = 0; i < source.length; i++) {
+//             if (source[i] instanceof MyArray && currentDepth > 0) {
+//                 flatten(source[i], currentDepth - 1);
+//             } else {
+//                 flatResult[flatResult.length] = source[i];
+//                 flatResult.length++;
+//             }
+//         }
+//     }
+
+//     flatten(this, depth);
+//     return flatResult;
+// };
+
+// const newMyArray = new MyArray(
+//     1,
+//     new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
+//     new MyArray()
+// );
+
+// console.log(newMyArray.flat(2));
+// console.log(newMyArray.flat(3));
+// console.log(newMyArray.flat(4));
+// console.log(newMyArray.flat(Infinity));
+// console.log(newMyArray.flat(0));
+
 function MyArray(...args) {
     this.length = args.length;
     for (let i = 0; i < args.length; i++) {
@@ -7,32 +44,29 @@ function MyArray(...args) {
     }
 }
 
-MyArray.prototype.flat = function (depth = 1) {
-    const flatResult = new MyArray();
-
-    function flatten(source, currentDepth) {
-        for (let i = 0; i < source.length; i++) {
-            if (source[i] instanceof MyArray && currentDepth > 0) {
-                flatten(source[i], currentDepth - 1);
-            } else {
-                flatResult[flatResult.length] = source[i];
-                flatResult.length++;
+function MyArrayPrototype() {
+    this.push = function (...args) {
+        if (args.length > 0) {
+            for (let i = 0; i < args.length; i++) {
+                this[this.length] = args[i];
+                this.length++;
             }
         }
-    }
+        return this.length;
+    };
+    this.forEach = function (callback, thisArg) {
+        for (let i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
 
-    flatten(this, depth);
-    return flatResult;
-};
+MyArray.prototype = new MyArrayPrototype();
 
-const newMyArray = new MyArray(
-    1,
-    new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
-    new MyArray()
-);
+const newArr = new MyArray();
 
-console.log(newMyArray.flat(2));
-console.log(newMyArray.flat(3));
-console.log(newMyArray.flat(4));
-console.log(newMyArray.flat(Infinity));
-console.log(newMyArray.flat(0));
+console.log(newArr.push(100));
+
+console.log(newArr);
+
+newArr.forEach((item) => console.log(item + 99));
