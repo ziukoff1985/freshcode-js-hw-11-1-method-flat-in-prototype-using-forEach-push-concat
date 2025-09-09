d5564dbbf17486c177b4a95d1f73f600b5a3259c
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-09 11:12:41 +0300

    Fix method flat logic

--- src/index.js
+++ src/index.js
@@ -1,42 +1,5 @@
 'use strict';
 
-// function MyArray(...args) {
-//     this.length = args.length;
-//     for (let i = 0; i < args.length; i++) {
-//         this[i] = args[i];
-//     }
-// }
-
-// MyArray.prototype.flat = function (depth = 1) {
-//     const flatResult = new MyArray();
-
-//     function flatten(source, currentDepth) {
-//         for (let i = 0; i < source.length; i++) {
-//             if (source[i] instanceof MyArray && currentDepth > 0) {
-//                 flatten(source[i], currentDepth - 1);
-//             } else {
-//                 flatResult[flatResult.length] = source[i];
-//                 flatResult.length++;
-//             }
-//         }
-//     }
-
-//     flatten(this, depth);
-//     return flatResult;
-// };
-
-// const newMyArray = new MyArray(
-//     1,
-//     new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
-//     new MyArray()
-// );
-
-// console.log(newMyArray.flat(2));
-// console.log(newMyArray.flat(3));
-// console.log(newMyArray.flat(4));
-// console.log(newMyArray.flat(Infinity));
-// console.log(newMyArray.flat(0));
-
 function MyArray(...args) {
     this.length = args.length;
     for (let i = 0; i < args.length; i++) {
@@ -84,45 +47,70 @@ function MyArrayPrototype() {
         const flatArr = new MyArray();
 
         function flatten(source, currentDepth) {
-            for (let i = 0; i < source.length; i++) {
-                if (source[i] instanceof MyArray && currentDepth > 0) {
-                    flatten(source[i], currentDepth - 1);
+            let tempArr = new MyArray();
+            source.forEach((item) => {
+                if (item instanceof MyArray && currentDepth > 0) {
+                    const child = flatten(item, currentDepth - 1);
+                    tempArr = tempArr.concat(child);
                 } else {
-                    flatArr.push(source[i]);
+                    tempArr.push(item);
                 }
-            }
+            });
+            return tempArr;
         }
-        flatten(this, depth);
-        return flatArr;
+
+        return flatArr.concat(flatten(this, depth));
     };
 }
 
 MyArray.prototype = new MyArrayPrototype();
 
-const newArr = new MyArray();
-
-const newArr2 = new MyArray(1, 2, 3);
-const newArr3 = new MyArray(undefined);
-
-console.log(newArr.push(100));
-
-console.log(newArr3);
-
-newArr.forEach((item) => console.log(item + 99));
-
-console.log(newArr.concat(100));
-
-console.log(newArr.concat(newArr2));
-console.log(newArr.concat(newArr3));
-
-console.log(newArr === newArr.concat());
-
-const newMyArray = new MyArray(
-    1,
+const newArr = new MyArray(
+    undefined,
     new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
     new MyArray()
 );
 
-console.log(newMyArray);
+console.log(newArr);
+console.log(newArr.flat(2));
+console.log(newArr.flat(3));
+console.log(newArr.flat(4));
+console.log(newArr.flat(Infinity));
+console.log(newArr.flat(0));
 
-console.log(newMyArray.flat(Infinity));
+function MyArray(...args) {
+    this.length = args.length;
+    for (let i = 0; i < args.length; i++) {
+        this[i] = args[i];
+    }
+}
+
+// MyArray.prototype.flat = function (depth = 1) {
+//     const flatResult = new MyArray();
+
+//     function flatten(source, currentDepth) {
+//         for (let i = 0; i < source.length; i++) {
+//             if (source[i] instanceof MyArray && currentDepth > 0) {
+//                 flatten(source[i], currentDepth - 1);
+//             } else {
+//                 flatResult[flatResult.length] = source[i];
+//                 flatResult.length++;
+//             }
+//         }
+//     }
+
+//     flatten(this, depth);
+//     return flatResult;
+// };
+
+// const newMyArray = new MyArray(
+//     1,
+//     new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
+//     new MyArray()
+// );
+
+// console.log(newMyArray.flat(2));
+// console.log(newMyArray.flat(3));
+// console.log(newMyArray.flat(4));
+// console.log(newMyArray.flat(Infinity));
+// console.log(newMyArray.flat(0));
