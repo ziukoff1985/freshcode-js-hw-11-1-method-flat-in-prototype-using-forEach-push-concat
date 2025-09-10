81ff2903834afc7aa4b3dada9dd58275b3bf4bdb
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-10 10:20:39 +0300

    Finally

--- src/index.js
+++ src/index.js
@@ -61,68 +61,16 @@ MyArray.prototype = new MyArrayPrototype();
 
 const newArr = new MyArray(
     1,
-    new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
+    new MyArray(
+        2,
+        undefined,
+        new MyArray(undefined, 3, new MyArray(4, undefined))
+    ),
     new MyArray()
 );
 
-const newArr2 = new MyArray();
-const newArr3 = new MyArray(1, new MyArray(2));
-
-console.log(newArr2.concat(newArr3));
-// console.log(newArr2.push(1));
-// console.log(newArr2);
-
-console.log(newArr);
 console.log(newArr.flat());
+console.log(newArr.flat(2));
 console.log(newArr.flat(3));
-
-// function MyArray(...args) {
-//     this.length = args.length;
-//     for (let i = 0; i < args.length; i++) {
-//         this[i] = args[i];
-//     }
-// }
-
-// MyArray.prototype.flat = function (depth = 1) {
-//     const flatResult = new MyArray();
-
-//     function flatten(source, currentDepth) {
-//         for (let i = 0; i < source.length; i++) {
-//             if (source[i] instanceof MyArray && currentDepth > 0) {
-//                 flatten(source[i], currentDepth - 1);
-//             } else {
-//                 flatResult[flatResult.length] = source[i];
-//                 flatResult.length++;
-//             }
-//         }
-//     }
-
-//     flatten(this, depth);
-//     return flatResult;
-// };
-
-// const newMyArray = new MyArray(
-//     1,
-//     new MyArray(10, new MyArray(100, new MyArray(1000, new MyArray(10000)))),
-//     new MyArray()
-// );
-
-// console.log(newMyArray.flat(2));
-// console.log(newMyArray.flat(3));
-// console.log(newMyArray.flat(4));
-// console.log(newMyArray.flat(Infinity));
-// console.log(newMyArray.flat(0));
-
-const arr3 = [1, undefined, 3, 4];
-const arr4 = [5, 6, undefined, 8];
-console.log(arr3.concat(arr4));
-
-const arr5 = [1, [undefined, [3, undefined]]];
-
-console.log(arr5.flat());
-
-const arr6 = new MyArray(1, [undefined, [3, undefined]]);
-
-const arr7 = new MyArray(1, [undefined, [3, undefined]]);
-
-console.log(arr6.concat(arr7));
+console.log(newArr.flat(-1));
+console.log(newArr.flat(Infinity));
